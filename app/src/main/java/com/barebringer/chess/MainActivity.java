62346893afcac1b8aaeec9aac8b4b8bbca67781d
cheeni666 <vshreenivasbharadwaj@gmail.com>
2016-09-25 20:13:13 +0530

    1st version-icon

--- app/src/main/java/com/barebringer/chess/MainActivity.java
+++ app/src/main/java/com/barebringer/chess/MainActivity.java
@@ -2,9 +2,9 @@ package com.barebringer.chess;
 
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
